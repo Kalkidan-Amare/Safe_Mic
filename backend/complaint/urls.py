cc56485fa5ba082ad53381b304fcd6f65c9e31ff
kalkidan yishak <kalkidanyishak002@gmail.com>
2024-04-14 18:13:52 +0300

    complaints

--- backend/complaint/urls.py
+++ backend/complaint/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
+from .serializers import ComplaintListAPI
 from .views import *
 
 urlpatterns = [
     path('complaints/', ComplaintView.as_view(), name='complaints-list-create'),
+    path('all-complaints/', ComplaintList.as_view(), name='complaint-list'),
     path('complaints/summary/<str:tag>/', get_summary, name="summary"),
 ]
