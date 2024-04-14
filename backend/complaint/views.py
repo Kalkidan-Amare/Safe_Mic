be8a51441199271d182e4d2ad522f1c97649f64b
kalkidan yishak <kalkidanyishak002@gmail.com>
2024-04-14 18:12:52 +0300

    complaints

--- backend/complaint/views.py
+++ backend/complaint/views.py
@@ -10,13 +10,16 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .summarize import summarize
 from rest_framework.decorators import api_view
 from .openaisummarize import refine_summary
+from rest_framework.permissions import IsAdminUser
 
 class ComplaintView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def get(self, request):
-        # The authenticated user is available via request.user
+        if not request.user.is_staff:
+            return Response({"message": "Unauthorized"}, status=status.HTTP_403_FORBIDDEN)
+        
         complaints = Complaint.objects.filter(user=request.user)
         serializer = ComplaintSerializer(complaints, many=True)
         return Response(serializer.data)
@@ -35,21 +38,31 @@ class ComplaintView(APIView):
         self.check_object_permissions(request, complaint)
         complaint.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+
 @api_view(['GET'])
 def get_summary(request, tag):
     tag = tag.lower()
     # Retrieve complaints with the specified tag
     complaints = Complaint.objects.filter(tag=tag)
-    print(tag)
-    print("Number of complaints retrieved:", len(complaints))
-    # Combine complaint bodies into a single text
+    if not complaints:
+        return Response({"tag": tag, "summary": "no complaints available for this tag"})
+
     combined_text = " ".join(complaint.body for complaint in complaints)
-    # Summarize the combined text
-    print("Combined text:", combined_text)
     initial_summary = summarize(combined_text)
-    
-    # Refine initial summary using OpenAI's GPT
     refined_summary = refine_summary(initial_summary) 
-    print("Accepted Renderer:", request.accepted_renderer)
     return Response({"tag": tag, "summary": refined_summary})
+
+class ComplaintList(APIView):
+    # Ensure only staff users can access this view
+    permission_classes = [IsAdminUser]
+
+    def get(self, request, format=None):
+        # Check if the user is a staff member
+        if request.user.is_staff:
+            complaints = Complaint.objects.all()
+            serializer = ComplaintSerializer(complaints, many=True)
+            return Response(serializer.data)
+        else:
+            # Return an empty response for non-staff users
+            return Response([])
+
